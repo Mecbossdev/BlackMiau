a086e8c31eafed7f2c4beaeef6fb4c699bf0ba78
Jeferson Fernandes <jeferson.fernandesdasilva@hotmail.com>
2021-11-24 18:09:01 -0200

    Outro ponto Code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
 import Header from '../components/Header';
-import './styles.css';
+import Feed from '../components/Feed';
+import Navbar from '../components/Navbar';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/system/Box';
+//import './styles.css';
+
+const useStyles = makeStyles({
+  root: {
+    display:'flex',
+    flexDirection: 'column'
+  },
+  main: {
+    height: '100vh',
+    display: 'flex',
+    width: '1280px',
+    margin: '0 auto',
+  },
+  toolbar: {
+    minHeight: 64,
+  }
+});
 
 function Home() {
-  return (
-  <div>
-    <Header />
-    <main className='main'>
-      <div className='navbar'></div>
-      <div className='feed'>
+  const classes = useStyles();
 
-      </div>
+
+  return (
+  <div className = {classes.root}>
+    <Header />
+    <div className={classes.toolbar}></div>
+    <main className={classes.main}>
+      <Container>
+        <Box>
+          <Feed />
+          <Navbar />
+        </Box>
+      </Container>
+      
     </main>
+
   </div>
   );
 }
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,21 +1,64 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+import SvgIcon from '@mui/material/SvgIcon';
+import { Bell } from 'react-feather';
+import Avatar from '@mui/material/Avatar';
 
+const useStyles = makeStyles({
+  appBar: {
+    boxShadow: 'none',
+  },
+  img: {
+    maxheight: 35,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  userSection: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  button: {
+    marginRight: 10
+  },
+  bell: {
+    marginRight: 10
+  }
+
+});
 
 function Header() {
+   const classes = useStyles();
+
   return (
-    <header className='header'>
-        <div className='toolbar'>
-          <div>
+    <AppBar position="fixed" color="inherit" className={classes.appBar}>
+        <Toolbar>
+          <img src="images/log.png" alt="logo" className={classes.img}></img>
+          <div className={classes.grow}></div>
+          <div className={classes.userSection}>
+            <Button color="primary" variant="contained" className={classes.button}>
+              Novo Post
+            </Button>
+            <SvgIcon className={classes.bell}>
+              <Bell></Bell>
+            </SvgIcon>
+            <Avatar alt="Jeferson Fernandes" src="" />
+          </div>
+          {/*<div>
             <a href='/'>BlackMiau</a>
+            <input type="text"></input>
           </div>
           <div>
-            <button>Novo Post</button>
+            
             <span>img1</span>
             <span>img2</span>
-          </div>
-        </div>
-    </header>
+          </div>*/}
+        </Toolbar>
+    </AppBar>
     )
-  }
+  };
 
   export default Header;
